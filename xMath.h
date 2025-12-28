2933d7036d4c09e350d2b7a495797f39d0c90011
felixjaschul <felix@jaschul.de>
2025-12-28 02:01:23 +0100

    .

--- xMath.h
+++ xMath.h
@@ -12,43 +12,43 @@ typedef struct {
     Vec3 direction;
 } Ray;
 
-static inline Vec3 vec3(float x, float y, float z) {
+static inline Vec3 vec3(const float x, const float y, const float z) {
     return (Vec3){x, y, z};
 }
 
-static inline Vec3 add(Vec3 a, Vec3 b) {
+static inline Vec3 add(const Vec3 a, const Vec3 b) {
     return (Vec3){a.x + b.x, a.y + b.y, a.z + b.z};
 }
 
-static inline Vec3 sub(Vec3 a, Vec3 b) {
+static inline Vec3 sub(const Vec3 a, const Vec3 b) {
     return (Vec3){a.x - b.x, a.y - b.y, a.z - b.z};
 }
 
-static inline Vec3 mul(Vec3 v, float t) {
+static inline Vec3 mul(const Vec3 v, const float t) {
     return (Vec3){v.x * t, v.y * t, v.z * t};
 }
 
-static inline Vec3 vdiv(Vec3 v, float t) {
+static inline Vec3 vdiv(const Vec3 v, const float t) {
     return (Vec3){v.x / t, v.y / t, v.z / t};
 }
 
-static inline float dot(Vec3 a, Vec3 b) {
+static inline float dot(const Vec3 a, const Vec3 b) {
     return a.x * b.x + a.y * b.y + a.z * b.z;
 }
 
-static inline float len(Vec3 v) {
+static inline float len(const Vec3 v) {
     return sqrtf(dot(v, v));
 }
 
-static inline Vec3 norm(Vec3 v) {
+static inline Vec3 norm(const Vec3 v) {
     return vdiv(v, len(v));
 }
 
-static inline Vec3 reflect(Vec3 v, Vec3 n) {
+static inline Vec3 reflect(const Vec3 v, const Vec3 n) {
     return sub(v, mul(n, 2.0f * dot(v, n)));
 }
 
-static inline Vec3 cross(Vec3 a, Vec3 b) {
+static inline Vec3 cross(const Vec3 a, const Vec3 b) {
     return (Vec3){
         a.y * b.z - a.z * b.y,
         a.z * b.x - a.x * b.z,
