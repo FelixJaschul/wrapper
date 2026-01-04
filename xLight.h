d56e3a1bc1db3616a72e832b9045579676b7bfbb
felixjaschul <felix@jaschul.de>
2026-01-04 10:52:44 +0100

    .

---
+++ xLight.h
@@ -0,0 +1,36 @@
+#ifndef XLIGHT_H
+#define XLIGHT_H
+
+#include "xMath.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct xLight {
+    Vec3 position;
+    float intensity;
+} xLight;
+
+// Initialize light with default values
+/*  -> Example:
+ *  xLight light;
+ *  xLightInit(&light);
+ *  light.position = vec3(0.0f, 5.0f, 0.0f);
+ */
+void xLightInit(xLight *light);
+
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef XLIGHT_IMPLEMENTATION
+
+inline void xLightInit(xLight *light)
+{
+    light->position = vec3(0.0f, 0.0f, 0.0f);
+    light->intensity = 1.0f;
+}
+
+#endif // XLIGHT_IMPLEMENTATION
+#endif // XLIGHT_H
